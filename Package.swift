ac38828f5ee47f9025aad07790a836aaaff121bd
Wembley Leach <wembley@hey.com>
2021-04-27 08:25:12 -0400

    Use exact version 1.3.2 of CryptoSwift

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/krzyzanowskim/CryptoSwift.git",
-            .upToNextMinor(from: "1.3.2")
+            .exact(from: "1.3.2")
         )
     ],
     targets: [
